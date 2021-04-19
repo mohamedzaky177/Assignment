7d4c2219691c3616e54c6e98500c3550a79a90e1
zaky177 <mohamedzaky177@gmail.com>
2021-04-19 18:23:50 +0200

    modification to be like my other friends codes

--- zaky.c
+++ zaky.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int N;
-typedef struct node
-{
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+#include<conio.h>
+int j=0;
+struct student_info {
+    char name_first[20];
+    char name_last[20];
     int id;
-    char name[40];
-    int birth_year;
-    int birth_month;
-    int birth_day;
-    struct node *next;
-}
-node;
+    struct birthdate {
+        int day;
+        int month;
+        int year;
 
-void get_info(node  *n);
-void insertend(node **tailp);
-void insertbeg(node **headp);
-void insertmiddle(node *head, int x);
-void list (void);
-
-void get_infoarr(struct node  *arr);
-void array(void);
-void insertendarr(node *arr);
-void insertbegarr(node **a);
-void insertmidarr(node **a, int x);
-
-
+    }birth_date;
+    int last_year_score;
+};
+struct node {
+    struct student_info data;
+    struct node* next;
+};
+void append(struct student_info** head_ref);
+void insert(struct node* prev);
+void first(struct node** old);
+void clean(struct node** data);
+void index(struct node*data,int position);
+void calling();
+void push_first_array(struct student_info**h,int*n);
+void push_into_middle(struct student_info**h,int*n,int z);
+void insert_student_last(struct student_info**h,int*n);
+void print_array(struct student_info a[],int arraysize);
 int main()
 {
-    printf("hello to students Data structure program \n");
-    array();
-    list();
+    printf("\n welcome to our program, here u can save your students data  \n ");
+    calling();
+    return 0;
 }
