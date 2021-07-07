47c15edf558dcb046addb274169e8c701c572c9a
zaky177 <mohamedzaky177@gmail.com>
2021-07-07 20:27:45 +0200

    final edits

--- Assignment/zaky.c
+++ Assignment/zaky.c
@@ -1,206 +1,338 @@
-void append(struct student_info** head_ref){                        //this function adds data in the end of the list
-    system("cls");
-    j++;//its cost is 2T for incrementation and assignment 
-    struct node* new_student = (struct node*) malloc(sizeof(struct student_info));       //defining new node,it has a cost of 1T
-    struct node* last = *head_ref;                                                       //defining a pointer to the head,it has a cost of 1T
-    printf("\n student number %d name (first and last) : ", j);                          //entering new data
-    scanf("%s %s", new_student->data.name_first, new_student->data.name_last);
-    printf("\n student number %d id : ", j);
-    scanf("%d", &(new_student->data.id));
-    printf("\n student number %d score : ", j);
-    scanf("%d", &(new_student->data.last_year_score));
-    printf("\n student number %d birth date day/month/year ", j);
-    scanf("%d", &(new_student->data.birth_date.day));
-    scanf("%d", &(new_student->data.birth_date.month));
-    scanf("%d", &(new_student->data.birth_date.year));
-    new_student->next = NULL;                                                   //make the next node = NULL
-    while (last->next != NULL)
+#include <stdio.h>
+#include <stdlib.h>
+#include<conio.h>
+#include<string.h>
+
+int N;
+
+//The structure has a total size of 60 bytes
+typedef struct st
+{
+    int id;
+    char name[40];
+    int birth_year;
+    int birth_month;
+    int birth_day;
+}st;
+
+//one node has a total size of 72 bytes
+typedef struct node
+{
+    st stu;
+    struct node *next;
+}node;
+
+void get_info(node  *n);
+void insertend(node **tailp);
+void insertbeg(node **headp);
+void insertmiddle(node *head, int x);
+void list (void);
+
+void get_infoarr(struct st  *arr);
+void array(void);
+void insertendarr(st *arr);
+void insertbegarr(st **a);
+void insertmidarr(st **a, int x);
+
+
+int main()
+{
+    printf("hello to students Data structure program \n");
+    array();
+    list();
+}
+
+/* contructing the dynamic array */
+void array(void)
+{
+    N = 0;
+    printf("\nNumber of students intially: ");
+    scanf("%i" , &N);
+    fflush(stdin);
+    if( N== INT_MAX )
     {
-        last = last->next;                                                     //move from the head to the tail,it has a cost of 1T and n number of times while n is the number of elements of linked list
-    }
-    last->next = new_student;//has a cost of 1T                                                  //insert the new node in the tail
-    return;//has a cost of 1T 
-}//so it has a total cost of time =5T+1T*n;
-void insert(struct node* prev) {                                            //this function adds data in the middle of the list
-    j++;
-    system("cls");
-    if (prev == NULL)
-    {
-        printf("you have to enter some data first\n");
         return;
     }
-    int z = j % 2;                                                //trying to find out if the number of students is even or odd
-    struct node* temp = prev;
-    if (z == 0)
+    st *arr = malloc(sizeof(st)*N);
+    for (int i=0; i < N; i++)
     {
-        for (int i = 2; i <= (j / 2) - 1; i++)
-            temp = temp->next;
-    }                                                             //move from the head to the middle of the list
-    else {
-        for (int i = 2; i <= (j + 1) / 2 - 1; i++)
-            temp = temp->next;
+        printf("\nenter student %i info \n", i+1);
+        get_infoarr(arr+i);
     }
-    struct node* new_student = (struct node*) malloc(sizeof(struct student_info));      //defining new node with new data
-    printf("\n student number %d name (first and last) : ", j);
-    scanf("%s %s", new_student->data.name_first, new_student->data.name_last);
-    printf("\n student number %d id : ", j);
-    scanf("%d", &(new_student->data.id));
-    printf("\n student number %d score : ", j);
-    scanf("%d", &(new_student->data.last_year_score));
-    printf("\n student number %d birth date day/month/year : ", j);
-    scanf("%d", &(new_student->data.birth_date.day));
-    scanf("%d", &(new_student->data.birth_date.month));
-    scanf("%d", &(new_student->data.birth_date.year));
-    new_student->next = temp->next;                                 //inserting the new node
-    temp->next = new_student;
-}
-void first(struct node** old) {                                  //this function adds data in the first position of the list
-    j++;//its cost is 2T for incrementation and assignment 
-    system("cls");
-    struct node* new_student = (struct node*) malloc(sizeof(struct student_info));     //defining new node with new dataوit has a cost of 1T
-    printf("\n student number %d name (first and last) : ", j);
-    scanf("%s %s", new_student->data.name_first, new_student->data.name_last);
-    printf("\n student number %d id : ", j);
-    scanf("%d", &(new_student->data.id));
-    printf("\n student number %d score : ", j);
-    scanf("%d", &(new_student->data.last_year_score));
-    printf("\n student number %d birth date day/month/year : ", j);
-    scanf("%d", &(new_student->data.birth_date.day));
-    scanf("%d", &(new_student->data.birth_date.month));
-    scanf("%d", &(new_student->data.birth_date.year));
-    new_student->next = *old;                                              //inserting the new node,it has a cost of 1T
-    *old = new_student;                                                    //it has a cost of 1T
-}// SO sum of total time cost is equal to 5T
-void print(struct node* data) {                                     //this function print all the data in the list
-    system("cls");
-    while (data != NULL)
+    for (int i=0; i < N; i++)
     {
-        printf("\n %s %s", data->data.name_first, data->data.name_last);
-        printf("\n id: %d", data->data.id);
-        printf("\n score: %d", data->data.last_year_score);
-        printf("\n date of birth : %d/%d/%d", data->data.birth_date.day, data->data.birth_date.month, data->data.birth_date.year);
-        printf("\n ________________________________________\n");
-        data = data->next;
+        printf("%i\n", (arr+i) -> id);
     }
-}
-void clean(struct node** data) {                //this function clear the data of the list in the memory
-    struct node* current = *data;
-    struct node* next;
-    while (current != NULL) {
-        next = current->next;
-        free(current);
-        current = next;
-    }*data = NULL;
-}
-void index(struct node*data,int position){                //this function adds data at a specific position in the list
-    j++;//its cost is 2T for incrementation and assignment
-    system("cls");
-    struct node*temp=data;
-    struct node* new_student = (struct node*) malloc(sizeof(struct student_info));        //defining new node with new data,it has a cost of 1T
-    printf("\n student number %d name (first and last) : ", j);
-    scanf("%s %s", new_student->data.name_first, new_student->data.name_last);
-    printf("\n student number %d id : ", j);
-    scanf("%d", &(new_student->data.id));
-    printf("\n student number %d score : ", j);
-    scanf("%d", &(new_student->data.last_year_score));
-    printf("\n student number %d birth date day/month/year : ", j);
-    scanf("%d", &(new_student->data.birth_date.day));
-    scanf("%d", &(new_student->data.birth_date.month));
-    scanf("%d", &(new_student->data.birth_date.year));
-    if(temp!=NULL)
-        for(int i=1;i<position-1;i++)                        //moving from the head to the position,this line has a cost of 2T and numbers of times = position-1
-        temp=temp->next;                                      //this line has a cost of 1T and numbers of times = position-2
-    new_student->next = temp->next;                           //inserting the new node,has a cost of 1T
-    temp->next = new_student;                                 //has a cost of 1T
 
-}// so the the total cost of time is 6T+2T*(position-1)+1T*(position-2)
-void calling(){                                        //this function used to provide a menu to the user to use our program
-    struct node* head;
-    int sizearray = 0;
-    struct student_info* ptr = (struct student_info*)malloc(sizeof(struct student_info) * sizearray);
-    int menu=1;
-    int z;
-    int response = 1;
-    int m;
-    int number;
-    head = NULL;
-    printf("\n you can save the data in \n 1.linked list\n 2.dynamic array\n your response: ");
-    fflush(stdout);                                                //first u have to enter number of students and enter their data
-    scanf("%d", &menu);                                            //to create some data to make your operations
-    printf("\n number of students : ");
-    fflush(stdout);
-    scanf("%d", &number);
-    system("cls");
-    if (menu == 1){
-        while (j != number){
-        	if(j==0)
-            first(&head);
-        	else
-        		append(&head);
-        }}
-    else if (menu == 2){
-        while (sizearray != number){
-			if(sizearray==0)
-		push_first_array(&ptr, &sizearray);
-	        else
-			insert_student_last(&ptr, &sizearray);
-		  }}
-    while (response != 0 && menu != 0)                                      //here u can choose what is the function u want to call
+    insertbegarr(&arr);
+    insertendarr(arr);
+
+    int x;
+    printf("\nindex of new node (note that index start from 0 and ends at %i): ", N-1);
+    scanf("%i" , &x);
+    fflush(stdin);
+    if (x == N)
     {
-        if (menu == 1)
+        insertendarr(arr);
+    }
+    else if(x == 0)
+    {
+        insertbegarr(&arr);
+    }
+    else
+    {
+        insertmidarr(&arr ,x);
+    }
+
+    for (int i=0; i < N; i++)
+    {
+        printf("%i\n", (arr+i) -> id);
+    }
+    free(arr);
+
+
+}
+
+/* contructing the linked list */
+
+void list (void)
+{
+    N = 0;
+    node *head = NULL;
+    node *tail = NULL;
+    printf("\nNumber of students intially: ");
+    scanf("%i" , &N);
+    fflush(stdin);
+    if( N== INT_MAX )
+    {
+        return;
+    }
+
+    for (int i=0; i < N; i++)
+    {
+        node *n = malloc (sizeof(node));
+        if (!n)
         {
-            printf("\n 0.to exit and clear your data");
-            printf("\n 1.add a student at the beginning of your list ");
-            printf("\n 2.add a student in the middle of your list");
-            printf("\n 3.add a student at a specific position ");
-            printf("\n 4.add a student at the end of your list");
-            printf("\n 5.view your data");
-            printf("\n your response: ");
-            fflush(stdout);
-            scanf("%d", &response);
-            if (response == 4)
-                append(&head);
-            else if (response == 1)
-                first(&head);
-            else if (response == 2)
-                insert(head);
-            else if (response == 5)
-                print(head);
-            else if (response == 3)
-            {printf("\n enter the index : ");
-             scanf("%d",&m);
-             index(head,m);
-            }
-            else if (response == 0)
-                break;
-        }
-        else if (menu == 2)
-        {
-            printf("\n 0.to exit and clear your data");
-            printf("\n 1.add a student at the beginning of your array ");
-            printf("\n 2.add a student in a certain position of your array");
-            printf("\n 3.add a student at the end of your array");
-            printf("\n 4.view your array data");
-            printf("\n your response: ");
-            fflush(stdout);
-            scanf("%d", &response);
-            if (response == 1)
-                push_first_array(&ptr, &sizearray);
-            else if (response == 2){
-            	printf("\nplease enter the position you want to insert your student into :(starting from zero) ");
-            	fflush(stdout);
-            	scanf("%d",&z);
-                push_into_middle(&ptr, &sizearray,z);
-            }
-            else if (response == 3)
-                insert_student_last(&ptr, &sizearray);
-            else if (response == 4)
-                print_array(ptr, sizearray);
-            else if (response == 0)
-                break;
+            return;
         }
 
-    }free(ptr);
-    clean(&head);
-     return ;
+        printf("\nenter student %i info \n", i+1);
+        get_info(n);
+
+        if (head)
+        {
+            for (node *ptr = head; ptr != NULL; ptr = ptr -> next )
+            {
+                if(ptr-> next == NULL)
+                {
+                   ptr-> next = n;
+                   break;
+                }
+            }
+        }
+        else
+        {
+            head = n;
+        }
+
+        if ( i == N-1 )
+        {
+            tail = n;
+        }
+    }
+
+    insertbeg(&head);
+    insertend(&tail);
+    int x;
+    printf("\nindex of new node (note that index start from 0 and ends at %i): ", N-1);
+    scanf("%i" , &x);
+    fflush(stdin);
+    if (x == N)
+    {
+        insertend(&tail);
+    }
+    else if (x == 0)
+    {
+        insertbeg(&head);
+    }
+    else
+    {
+        insertmiddle(head,x);
+    }
+
+    for (node *ptr = head; ptr != NULL; ptr = ptr -> next )
+    {
+        printf("%i\n", ptr -> stu.id);
+    }
+
+
+
+    /* deleting allocated memory */
+    node *ptr = head;
+    while(ptr != NULL)
+    {
+        node *nxt = ptr -> next;
+        free(ptr);
+        ptr = nxt;
+    }
+}
+
+/* functions of list */
+
+void get_info(struct node  *n)
+{
+
+        printf("student id: ");
+        scanf("%i" , &(n->stu.id));
+        fflush(stdin);
+        printf("student birth year : ");
+        scanf("%i" , &(n->stu.birth_year));
+        fflush(stdin);
+        printf("student birth month : ");
+        scanf("%i" , &(n->stu.birth_month));
+        fflush(stdin);
+        printf("student birth day : ");
+        scanf("%i" , &(n->stu.birth_day));
+        fflush(stdin);
+
+        n -> next = NULL;
+
+        printf("student name: ");
+        scanf("%s" , (n->stu.name));
+        fflush(stdin);
+}
+
+void insertbeg(node **headp)
+{
+        node *n = malloc (sizeof(node));
+        if (!n)
+        {
+            return;
+        }
+
+        printf("\nenter student info \n");
+        get_info(n);
+        n -> next = *headp;
+        *headp = n;
+        N++;
+}
+
+void insertend(node **tailp)
+{
+        node *tail = *tailp;
+        node *n = malloc (sizeof(node));
+        if (!n)
+        {
+            return;
+        }
+
+        printf("\nenter student info \n");
+        get_info(n);
+        tail -> next = n;
+        *tailp = n;
+        N++;
+}
+
+void insertmiddle(node *head, int x)
+{
+
+        if (x >= N || x <= 0)
+        {
+            printf("index is not correct\n");
+            return;
+        }
+
+        node *n = malloc (sizeof(node));
+        if (!n)
+        {
+            return;
+        }
+        printf("\nenter student info \n");
+        get_info(n);
+        node *pre = head;
+        for (int k = 0; k < x-1; k++)
+        {
+            pre = pre -> next;
+        }
+        n -> next = pre -> next;
+        pre -> next = n;
+        N++;
+}
+
+/* functions of array */
+
+void get_infoarr(struct st  *arr)
+{
+
+    printf("student id: ");
+    scanf("%i" , &(arr->id));
+    fflush(stdin);
+    printf("student birth year : ");
+    scanf("%i" , &(arr->birth_year));
+    fflush(stdin);
+    printf("student birth month : ");
+    scanf("%i" , &(arr->birth_month));
+    fflush(stdin);
+    printf("student birth day : ");
+    scanf("%i" , &(arr->birth_day));
+    fflush(stdin);
+    printf("student name: ");
+    scanf("%s" , (arr->name));
+    fflush(stdin);
+
+}
+
+void insertendarr(st *arr)
+{
+    N++;
+    arr = realloc(arr,sizeof(st)*N);
+    printf("\nenter student info \n");
+    get_infoarr(arr+N-1);
+}
+void insertbegarr(st **a)
+{
+    N++;
+    st *temp = malloc(sizeof(st)*N);
+    for (int i=0; i < N-1; i++)
+    {
+        (temp + i + 1)->birth_day = ( (*a) + i )->birth_day;
+        (temp + i + 1)->birth_month = ( (*a) + i )->birth_month;
+        (temp + i + 1)->birth_year = ( (*a) + i )->birth_year;
+        (temp + i + 1)->id = ( (*a) + i )->id;
+        strcpy((temp + i + 1) -> name , ( (*a) + i ) -> name ) ;
+    }
+    free(*a);
+    printf("\nenter student info \n");
+    get_infoarr(temp);
+    *a = temp;
+}
+void insertmidarr(st **a, int x)
+{
+    if (x >= N || x <= 0)
+    {
+        printf("index is not correct\n");
+        return;
+    }
+    N++;
+    st *temp = malloc(sizeof(st)*N);
+    for (int i=0; i < x; i++)
+    {
+        (temp + i )->birth_day = ( (*a) + i )->birth_day;
+        (temp + i )->birth_month = ( (*a) + i )->birth_month;
+        (temp + i )->birth_year = ( (*a) + i )->birth_year;
+        (temp + i )->id = ( (*a) + i )->id;
+        strcpy((temp + i + 1) -> name , ( (*a) + i ) -> name ) ;
+    }
+    printf("\nenter student info \n");
+    get_infoarr(temp + x);
+    for (int i = x + 1; i < N; i++)
+    {
+        (temp + i )->birth_day = ( (*a) + i - 1)->birth_day;
+        (temp + i )->birth_month = ( (*a) + i - 1)->birth_month;
+        (temp + i )->birth_year = ( (*a) + i - 1)->birth_year;
+        (temp + i )->id = ( (*a) + i - 1)->id;
+        strcpy((temp + i + 1) -> name , ( (*a) + i - 1 ) -> name ) ;
+    }
+    free(*a);
+    *a = temp;
 }
